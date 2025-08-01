0c2f11394430eed60e50002fac9e65139967fb49
mth78260 <168089137+mth78260@users.noreply.github.com>
2025-08-01 16:08:53 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,11 @@
+export function middleware(req) {
+  const ua = req.headers.get("user-agent") || "";
+  const isTikTok = ua.includes("TikTok") || ua.includes("com.ss.android.ugc");
+
+  if (!isTikTok) {
+    return Response.redirect("https://t.me/+yOEXTSd298c3NTM0", 302);
+  }
+
+  return; // laisser TikTok charger la page
+}
 
